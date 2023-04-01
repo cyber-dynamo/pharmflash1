1dec75b83d19107186e77559011e4063a1d5ccb9
Paco Chan <pacochan@Pacos-Air.hitronhub.home>
2023-03-31 21:37:37 -0700

    Updates

---
+++ precache-manifest.9b9169cdcf4776ad1459ef2448be8021.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "774aa79506da48eedb63",
+    "url": "/pharmflash1/static/css/main.89e92839.chunk.css"
+  },
+  {
+    "revision": "774aa79506da48eedb63",
+    "url": "/pharmflash1/static/js/main.774aa795.chunk.js"
+  },
+  {
+    "revision": "82f3f0cc799277e00c27",
+    "url": "/pharmflash1/static/js/1.82f3f0cc.chunk.js"
+  },
+  {
+    "revision": "518f55b70f711b857267",
+    "url": "/pharmflash1/static/css/2.af75448c.chunk.css"
+  },
+  {
+    "revision": "518f55b70f711b857267",
+    "url": "/pharmflash1/static/js/2.518f55b7.chunk.js"
+  },
+  {
+    "revision": "3e63f08d15c309af7c5b",
+    "url": "/pharmflash1/static/js/runtime~main.3e63f08d.js"
+  },
+  {
+    "revision": "2a3de3e7ed98ebd169181c4097d3b4e6",
+    "url": "/pharmflash1/index.html"
+  }
+];
